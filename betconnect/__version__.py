d76e7bcde90ec3aed6f06ea6501755ccddb18751
liampauling <paulingliam@gmail.com>
2025-01-10 08:38:14 +0000

    upgrade requests<2.33.0

--- betconnect/__version__.py
+++ betconnect/__version__.py
@@ -1,6 +1,6 @@
 __title__ = "betconnect"
 __description__ = "A betconnect API client"
 __url__ = "https://github.com/betcode-org/betconnect"
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __author__ = "Oliver Varney"
 __license__ = "MIT"
